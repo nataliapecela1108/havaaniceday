255a64c846067c03a03174d0b2a78a208fe25877
nataliapecela1108 <116489503+nataliapecela1108@users.noreply.github.com>
2022-10-30 15:19:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+{/* <h1 class="banner-title">Good Morning</h1>
+<h3 class="banner-subtitle">Hello John</h3> */}
+
+const h1 = document.querySelector (".banner-title");
+
+h1.textContent = "Have a nice day !";
+h1.style.color = "green";
+
+const h3 = document.querySelector (".banner-subtitle");
+
+h3.textContent = "Hello Natalia !";
+h3.style.color = "yellow";
+
+
